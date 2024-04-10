53bb4bae89606c1f023b2036d532a4812b130f52
dokhactu <dokhactu1822@gmail.com>
2024-04-11 05:23:18 +0700

    update để đẩy host

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
-
+const { v4: uuidv4 } = require('uuid');
 
 // default options
 app.use(fileUpload());
@@ -24,7 +24,8 @@ configViewEngine(app);
 const oneDay = 1000 * 60 * 60 * 24;     // lưu phiên trong 1 ngày
 app.use(session({
     genid: function(req) {
-        return new Date().getTime()
+        // return new Date().getTime()
+        return uuidv4(); // Sử dụng UUID v4 để sinh giá trị duy nhất cho phiên
     },
     secret: 'secret-key',  // Chuỗi bí mật để mã hóa phiên
     saveUninitialized: true,
@@ -45,27 +46,27 @@ app.use('/', homeRoutes);
 app.use('/api/v1/', homeAPIRoutes);
 
 // -------  đoạn này là config chạy đẩy hosting lên domain
-// connection()
-// //test connection
-// app.listen(port, () => {
-//     console.log(`App listening on port ${port}`);
-//     console.log(`ĐÃ CHẠY ...   >>>  http://localhost:${port}`)
-//   });
+connection()
+//test connection
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+    console.log(`ĐÃ CHẠY ...   >>>  http://localhost:${port}`)
+  });
 
 
 // ------- đoạn dưới là connect db làm như bthg khi chạy docker
 //test connection
-(async () => {
-    try {
-        // using mongoose
-        await connection()
+// (async () => {
+//     try {
+//         // using mongoose
+//         await connection()
 
-        app.listen(port, hostname, () => {
-            console.log(`http://localhost:${port}`)
-        })
-    } catch(error) {
-        console.log(">>> LỖI RỒI CỤ: ", error);
-    }  
-})();
+//         app.listen(port, hostname, () => {
+//             console.log(`http://localhost:${port}`)
+//         })
+//     } catch(error) {
+//         console.log(">>> LỖI RỒI CỤ: ", error);
+//     }  
+// })();
 
 
